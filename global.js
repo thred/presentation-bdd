463f0304fa5d4d1762b463233e356e5d61b559e7
Manfred Hantschel <manfred.hantschel@porscheinformatik.at>
2015-10-22 09:15:16 +0200

    fixed text display

--- global.js
+++ global.js
@@ -1,5 +1,18 @@
 (function() {
 
+    document.addEventListener('keydown', function(event) {
+        if (event.keyCode === 73) {
+            event.preventDefault();
+            toggleText();
+        }
+    }, false);
+
+    function toggleText() {
+        var classList = document.body.classList;
+
+        classList.toggle('text-shown');
+        classList.toggle('text-hidden');
+    }
 
     Reveal.addEventListener('ready', function(event) {
         Reveal.addEventListener('slidechanged', function(event) {
